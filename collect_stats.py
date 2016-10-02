c03039724543bea92fcef32764e14f76e36d005a
tahabi <abizerkl@gmail.com>
2016-10-02 01:04:45 -0700

    fixes

--- collect_stats.py
+++ collect_stats.py
@@ -1,4 +1,5 @@
-import sys, os
+import sys
+import os
 import lxml.html
 import csv
 from collections import defaultdict
@@ -9,32 +10,39 @@ stat_files = [fn for fn in os.listdir('fields') if fn.startswith('print_')]
 countries = defaultdict(dict)
 
 # extract field name
+
+
 def get_field_name(lxmlobj):
-	field_name = lx.xpath('//*[@class="region"]')[0].getchildren()[0].text.replace(':', '').strip()
-	return unicode(field_name)
+    field_name = lx.xpath(
+        '//*[@class="region"]')[0].getchildren()[0].text.replace(':', '').strip()
+    return unicode(field_name)
 
 # extract the country name and stat value
+
+
 def countrystat(lxmlobj):
-	#lx = lxmlobj.xpath('//*[@class="fl_region"]')[0]
-	country = lxmlobj.getchildren()[0].text.strip()
-	stat = lxmlobj.getnext().text_content().strip()
-	return unicode(country).encode('ascii', 'ignore'), unicode(stat).encode('ascii', 'ignore')
+    country = lxmlobj.getchildren()[0].text.strip()
+    stat = lxmlobj.getnext().text_content().strip()
+    return unicode(country).encode('ascii', 'ignore'), unicode(stat).encode('ascii', 'ignore')
 
 # generate the csvs
 for fn in stat_files:
-	with open(os.path.join('fields', fn)) as statfile:
-		lx = lxml.html.fromstring(statfile.read())
-		field_name = get_field_name(lx)
-		with open(os.path.join('stat-out', "".join(x for x in field_name if x.isalnum()).replace(' ', '_').lower().strip() + '.csv'), 'w') as csvout:
-			csvwrite = csv.writer(csvout, delimiter = ',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
-			country_list = lx.xpath('//*[@class="fl_region"]')
+    with open(os.path.join('fields', fn)) as statfile:
+        lx = lxml.html.fromstring(statfile.read())
+        field_name = get_field_name(lx)
+        fname = "".join(x for x in field_name if x.isalnum()).lower().strip() + '.csv'
+        print "printing CSV for", field_name
+        with open(os.path.join('stat-out', fname), 'w') as csvout:
+            csvwrite = csv.writer(csvout, delimiter=',',
+                                  quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            country_list = lx.xpath('//*[@class="fl_region"]')
 
-			for country in country_list:
-				c, stat = countrystat(country)
-				print "printing", field_name, c, stat[:10]
-				csvwrite.writerow([c, stat])
-				countries[c.lower()][field_name] = stat
+            for country in country_list:
+                c, stat = countrystat(country)
+                csvwrite.writerow([c, stat])
+                countries[c.lower()][field_name] = stat
 
 for country in countries:
-	with open(os.path.join('country-out', country + '.json'), 'w') as cout:
-		cout.write(json.dumps(countries[country], indent = 2, sort_keys = True))
+    print "printing JSON for", country
+    with open(os.path.join('country-out', country.replace(' ', '_') + '.json'), 'w') as cout:
+        cout.write(json.dumps(countries[country], indent=2, sort_keys=True))
